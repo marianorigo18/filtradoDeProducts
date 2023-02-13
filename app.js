b2732b2693146d0241aab006a936a88e34c22465
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-13 16:23:41 -0300

    first commit

--- app.js
+++ app.js
@@ -1,40 +1,99 @@
-const $form = document.querySelector('#form');
-const $input = document.querySelector('#input');
-const $sugestions = document.querySelector('#sugestions');
-import {categories} from './exports/data.js';
-asignarListeners()
+const inputText = document.querySelector('#inputText');
 
-let sugerencias = [];
+const listGroup = document.querySelector('#sugestions');
 
-function asignarListeners(){
-    $form.addEventListener('keyup', capturarData)
-    // $input.addEventListener('keyup', showElements)
-}
+let item = listGroup.querySelectorAll('li');
 
-function capturarData(e){
-    e.preventDefault()
-    if($input.value == ""){
-        console.log('No se permiten valores vacios');
-    }else{
-        filtrarElementos($input.value)
+inputText.addEventListener('focus', () => {
+    if(!listGroup){
+        return;
     }
-    // $form.reset();
-}
-
-function filtrarElementos(param){
-    categories.map((elemento)=>{
-        let result = elemento.categories.filter(elemento => elemento.title.includes(param))
-        if(result.length >= 1){
-            for(let item of result){
-                sugerencias.push(item)
-                showElements(sugerencias)
-                return;
-            }
-        }
+    Array.from(item).forEach(element => {
+        element.style.display = 'block';
     })
-}
+})
 
-function showElements(item){
-    console.log(item)
-    // $sugestions.innerHTML =  `<li><a href="${item.link}">${item.title}</a></li>`;
-}
+inputText.addEventListener('keydown', (e) => {
+    let itemActual = Array.from(item).findIndex(item => item.classList.contains('active'));
+    if(e.keyCode==13){
+        e.preventDefault();
+        if(item[itemActual]){
+            item[itemActual].click();
+        }
+    }if(e.keyCode==38 || e.keyCode==40){
+        if(item[itemActual]){
+            item[itemActual].classList.remove('active');
+        }
+        itemActual += (e.keyCode == 38) ? -1 : 1;
+
+        if(itemActual < 0){
+            itemActual = 0
+        }else if(itemActual >= item.length){
+            itemActual = item.length -1;
+        }
+        item[itemActual].classList.add('active');
+    }
+})
+
+inputText.addEventListener('blur', () => {
+    let item = listGroup.querySelectorAll('li');
+    Array.from(item).forEach(element => {
+        element.style.display = '';
+    })
+})
+
+// console.log(inputText);
+// console.log(listGroup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const $form = document.querySelector('#form');
+// const $input = document.querySelector('#input');
+// const $sugestions = document.querySelector('#sugestions');
+// import {categories} from './exports/data.js';
+// asignarListeners()
+
+// let sugerencias = [];
+
+// function asignarListeners(){
+//     $form.addEventListener('keyup', capturarData)
+//     // $input.addEventListener('keyup', showElements)
+// }
+
+// function capturarData(e){
+//     e.preventDefault()
+//     if($input.value == ""){
+//         console.log('No se permiten valores vacios');
+//     }else{
+//         filtrarElementos($input.value)
+//     }
+//     // $form.reset();
+// }
+
+// function filtrarElementos(param){
+//     categories.map((elemento)=>{
+//         let result = elemento.categories.filter(elemento => elemento.title.includes(param))
+//         if(result.length >= 1){
+//             for(let item of result){
+//                 sugerencias.push(item)
+//                 showElements(sugerencias)
+//                 return;
+//             }
+//         }
+//     })
+// }
+
+// function showElements(item){
+//     console.log(item)
+//     // $sugestions.innerHTML =  `<li><a href="${item.link}">${item.title}</a></li>`;
+// }
